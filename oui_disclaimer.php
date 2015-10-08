a8993a570e7ba24191295875a37a94b1984fc80b
NicolasGraph <contact@nicolasmorand.com>
2015-10-08 11:11:36 +0200

    Revert "remove"
    This reverts commit 2814452d4558373352f227eaaa925dc573a73eeb.

--- oui_disclaimer.php
+++ oui_disclaimer.php
@@ -4,7 +4,7 @@ $plugin['name'] = 'oui_disclaimer';
 
 $plugin['allow_html_help'] = 0;
 
-$plugin['version'] = '1.2.0';
+$plugin['version'] = '1.1.1';
 $plugin['author'] = 'Nicolas Morand';
 $plugin['author_uri'] = 'http://www.nicolasmorand.com';
 $plugin['description'] = 'PHP powered disclaimer with cookie setting';
@@ -74,30 +74,35 @@ bc. <txp:oui_disclaimer />
 
 h3(#attributes). Attributes
 
-If used as a single tag, @<txp:oui_disclaimer />@ should contains at least a @message@ attribute. 
+If used as a single tag, @<txp:oui_disclaimer />@ should contains at least a @message@ or a @form@ attribute; @form@ overrides @message@. 
 
 * @cookie=""@ - _Default: oui_disclaimer_accepted_ - Name of the cookie set to hide the disclaimer for a defined duration.
 * @expires="…"@ - _Default: +1 week_ - The duration assigned to the cookie ("strtotime":http://php.net/manual/fr/function.strtotime.php valid value). 
+
+* @form=""@ - _Default: unset_ - The form used for the disclaimer content.
 
 * @wraptag="…"@ - _Default: div_ - The HTML tag used around the generated content.
 * @class="…"@ – _Default: oui_disclaimer_ - The css class to apply to the HTML tag assigned to @wraptag@. 
 
 * @label="…"@ – _Default: unset_ - The label used to entitled the generated content.
-* @labeltag="…"@ - _Default: h1_ - The HTML tag used around the value assigned to @label@.
+* @label_tag="…"@ - _Default: h1_ - The HTML tag used around the value assigned to @label@.
+* @label_class="…"@ – _Default: oui_disclaimer_label_ - The css class to apply to the HTML tag assigned to @label_tag@. 
 
-* @message="…"@ – _Default: unset_ - The main message to display.
-* @alt="…"@ – _Default: unset_ - The alternative message to display when the disclaimer is accepted.
+* @message="…"@ – _Default: unset_ - The main message you want to display.
+* @message_tag="…"@ - _Default: span_ - The HTML tag used around the value assigned to @message@.
+* @message_class="…"@ – _Default: oui_disclaimer_message_ - The css class to apply to the HTML tag assigned to @message_tag@.
 
-* @accept_url="…"@ - _Default: unset (current page)_ - An url to redirect the user once the discliamer accepted.
+* @alt_url="…"@ – _Default: unset_ - An alternative url to the @accept@ link used by the @alt@ link. 
+* @alt="…"@ – _Default: Read more_ - The text value to assigned to the @alt@ link.
+* @alt_class="…"@ – _Default: oui_disclaimer_alt_ - The css class to apply to the HTML link tag @a@.
+
 * @accept="…"@ - _Default: accept and continue_ - The text value to assigned to the @accept@ link.
-
-* @decline_url="…"@ – _Default: unset_ - An alternative url to the @accept@ link used by the @alt@ link. 
-* @decline="…"@ – _Default: Read more_ - The text value to assigned to the @alt@ link if a @decline_url@ is provided.
+* @accept_class="…"@ – _Default: oui_disclaimer_accept_ - The css class to apply to the HTML link tag @a@.
 
 h3. <txp:oui_disclaimer_accept />
 
 Displays a link to accept/hide the disclaimer message.
-Should be used only in @<txp:oui_disclaimer></txp:oui_disclaimer>@.
+Should be used only in the form used as the @form@ attribute of @<txp:oui_disclaimer />@.
 
 bc. <txp:oui_disclaimer_accept />
 
@@ -118,11 +123,15 @@ bc.. <div class="cookies-warning">
 h3. Exemple 2: container tag use 
 
 bc.. <txp:oui_disclaimer>
-	This content is crazy!
+	Your content
 	<txp:oui_disclaimer_accept />
-<txp:else />
-	Well, you are crazy…
 </txp:oui_disclaimer>
+
+h3. Exemple 3: Use a form 
+
+bc. <txp:oui_disclaimer form="cookies-warning" />
+
+Placed in your page(s), the code above will return the content of the form named _disclaimer_. This form must contains a @<txp:oui_disclaimer_accept />@ tag.
 
 h2(#author). Author
 
@@ -139,62 +148,42 @@ This plugin is distributed under "GPLv3":http://www.gnu.org/licenses/gpl-3.0.fr.
 # --- BEGIN PLUGIN CODE ---
 
 function oui_disclaimer($atts, $thing=null) {
-	global $oui_disclaimer_cookie, $oui_disclaimer_content;
+	global $oui_disclaimer_cookie, $oui_disclaimer_form, $oui_disclaimer_content;
 
 	extract(lAtts(array(
 		'cookie'  => 'oui_disclaimer_hidden',
 		'expires'  => '+1 week',
-		'wraptag'  => 'p',
-		'class'  => 'oui_disclaimer_content',
+		'form'  => null,
+		'wraptag'  => 'div',
+		'class'  => 'oui_disclaimer',
 		'label'  => '',
-		'labeltag'  => 'h1',
+		'label_tag'  => 'h1',
+		'label_class'  => 'oui_disclaimer_label',
 		'message'  => null,
-		'alt'  => '',
-		'accept_url'  => '',
+		'message_tag'  => 'span',
+		'message_class'  => 'oui_disclaimer_message',
 		'accept'  => gTxt('oui_disclaimer_accept'),
-		'decline_url'  => '',
-		'decline'  => gTxt('oui_disclaimer_alt'),
+		'accept_class'  => 'oui_disclaimer_accept',
+		'alt_url'  => '',
+		'alt'  => gTxt('oui_disclaimer_alt'),
+		'alt_class' => 'oui_disclaimer_alt',
 	),$atts));
 
 	$oui_disclaimer_cookie = $cookie;
+	$oui_disclaimer_form = $form;
 	$oui_disclaimer_content = $thing;
 
-	// HTTP GET or POST request contains '$cookie'?
+	// HTTP GET or POST request contains 'oui_accept_disclaimer_warning'?
 	if (gps($cookie))
 	{
 		// Set a cookie
 		setcookie($cookie, 1, strtotime(''.$expires.''), '/');
-
-		if ($thing===null)
-		{
-			$out = ($alt ? $alt : '');
-			return ($wraptag ? doTag($out, $wraptag, $class) : $out);
-		}
-		
-		else
-		{
-			$result = ($cookie == 1) ? 1 : 0;
-			return parse(EvalElse($thing, $result));
-		}
-
 	}
 
 	// Otherwise, cookie already exists?
 	else if (cs($cookie))
 	{
-
-		if ($thing===null)
-		{
-			$out = ($alt ? $alt : '');
-			return ($wraptag ? doTag($out, $wraptag, $class) : $out);
-		}
-		
-		else
-		{
-			$result = ($cookie == 1) ? 1 : 0;
-			return parse(EvalElse($thing, $result));
-		}
-		
+		// Do nothing
 	}
 
 	// Otherwise…
@@ -203,17 +192,26 @@ function oui_disclaimer($atts, $thing=null) {
 
 		if ($thing===null)
 		{
-	
-			if ($message!==null)
-			{
-				$out = '<span class="oui_diclaimer_message">'.$message.'.</span>'.($decline_url ? href($decline, $decline_url, ' class="oui_disclaimer_decline"') : '').href($accept, ($accept_url ? $accept_url : '').'?'.$cookie.'=1', ' class="oui_disclaimer_accept"');
 				
-				return '<div class="oui_disclaimer">'.($label ? doLabel($label, $labeltag) : '').(($wraptag) ? doTag($out, $wraptag, $class) : $out).'</div>';
+			if ($form!==null) 
+			{		
+				return output_form(array("form"=>$form));			
+			}
+	
+			else if ($message!==null)
+			{
+				return 
+				'<'.$wraptag.' class="'.$class.'">'.n.
+					($label ? '<'.$label_tag.' class="'.$label_class.'">'.$label.'</'.$label_tag.'>' : '').n.
+					'<'.$message_tag.' class="'.$message_class.'">'.$message.'.</'.$message_tag.'>'.n.	
+					($alt_url ? href($alt, $alt_url, ' class="'.$alt_class.'"') : '').n.
+					href($accept, '?'.$cookie.'=1', ' class="'.$accept_class.'"').n.
+				'</'.$wraptag.'>'.n;
 			}
 	
 			else	
 			{
-				trigger_error("oui_disclaimer requires a message when used as a single tag");
+				trigger_error("oui_disclaimer requires a message or a form attribute when used as a single tag");
 				return;
 			}
 
@@ -221,8 +219,7 @@ function oui_disclaimer($atts, $thing=null) {
 
 		else 
 		{
-			$result = ($cookie == $cookie) ? 1 : 0;
-			return parse(EvalElse($thing, $result));			
+			return $thing;
 		}
 		
 	}
@@ -230,24 +227,26 @@ function oui_disclaimer($atts, $thing=null) {
 }
 
 function oui_disclaimer_accept($atts) {
-	global $oui_disclaimer_cookie, $oui_disclaimer_content;
+	global $oui_disclaimer_cookie, $oui_disclaimer_form, $oui_disclaimer_content;
 
 	extract(lAtts(array(
-		'url'  => '',
 		'wraptag' => '',
 		'class'  => 'oui_disclaimer_accept',
 		'link'  => gTxt('oui_disclaimer_accept'),
+		'link_class'  => 'oui_disclaimer_accept_link',
 	),$atts));
 
-	if ($oui_disclaimer_content===null || $oui_disclaimer_content!==null) 
+	if (($oui_disclaimer_content===null && $oui_disclaimer_form===null) || $oui_disclaimer_content!==null) 
 	{
-		$out =  href($link, ($url ? $url : '').'?'.$oui_disclaimer_cookie.'=1', ' class="oui_disclaimer_accept"');
-		return ($wraptag ? doWrap($out, $wraptag, $class) : $out);
+		return 
+		($wraptag ? '<'.$wraptag.' class="'.$class.'">' : '').n.
+			href($link, '?'.$oui_disclaimer_cookie.'=1', ' class="'.$link_class.'"').n.
+		($wraptag ? '</'.$wraptag.'>' : '').n;
 	}
 	
 	else 
 	{
-		trigger_error("oui_disclaimer_accept must be used in a oui_disclaimer container tag");
+		trigger_error("oui_disclaimer_accept must be used in a oui_disclaimer container tag or in a form assigned via the oui_disclaimer form attribute");
 		return;
 	}
 		
